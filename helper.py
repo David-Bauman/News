f46af3c7f73098f95c0fae3919847e8d691df447
David Bauman <davidnormanbauman@gmail.com>
2018-12-24 09:53:46 -0500

    Cleans up logic, follows naming conventions
    Better data structures, control flow

--- helper.py
+++ helper.py
@@ -1,100 +1,63 @@
-"""
-First two functions (tag_visible and text_from_html) are from user jbochi on
-stackoverflow: https://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text/1983219
-David Bauman, 2017 - 12 - 17
-"""
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 from urllib.request import *
+
+
 def tag_visible(element):
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
+    return not (element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]'] or isinstance(element, Comment))
+
 
 def text_from_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
+    soup = BeautifulSoup(body, 'lxml')
     texts = soup.find_all(text=True)
     visible_texts = filter(tag_visible, texts)
-    return u" ".join(t.strip() for t in visible_texts)
+    return " ".join(t.strip() for t in visible_texts)
+
 
 def keywords(url):
-    html = urlopen(url).read()
-    text = text_from_html(html)
-    lst = scrubit(text)
-    counts = assemblecounts(lst)
+    lst = clean(text_from_html(urlopen(url).read()))
+    counts = assemble_counts(lst)
 
-    best = ["",0]
-    for sublist in counts:
-        if sublist[1] > best[1]:
-            best = sublist
+    best = []
+    for i in range(3):
+        temp = max(counts, key=lambda key: counts[key])
+        best.append(temp)
+        counts[temp] = -1
 
-    best2 = ["",0]
-    for sublist in counts:
-        if sublist == best:
-            pass
-        elif sublist[1] > best2[1]:
-            best2 = sublist
+    return best, len(lst)
 
-    best3 = ["",0]
-    for sublist in counts:
-        if sublist == best or sublist == best2:
-            pass
-        elif sublist[1] > best3[1]:
-            best3 = sublist
 
-    return best,best2,best3,int(len(lst))
-
-def assemblecounts(lst):
-    counts = []
+def assemble_counts(lst):
+    counts = {}
     for word in lst:
-        if len(word) < 5:
-            pass
-        elif word == "continue" or word == "Continue":
-            pass
+        if len(word) < 5 or word.title() == "Continue":
+            continue
+        if word in counts:
+            counts[word] += 1
         else:
-            low = 0
-            high = len(counts) - 1
-            Valid = False
-            while low <= high:
-                middle = int((low + high)/2)
-                if word == counts[middle][0]:
-                    Valid = True
-                    break
-                elif word > counts[middle][0]:
-                    low = middle + 1
-                else:
-                    high = middle -1
-            if Valid:
-                counts[middle][1] += 1
-            else:
-                counts.insert(low,[word,1])
+            counts[word] = 1
     return counts
 
-def scrubit(line):
-    Valid = True
+
+def clean(line):
     cleanline = ""
     acc = 0
     for i in range(len(line)):
         ch = line[i]
         if ch == "W":
-            if line[i+1:i+47] == "e’re interested in your feedback on this page.":
-                Valid = False
-        if ch =="F" and acc == 1:
+            if line[i:i+47] == "We’re interested in your feedback on this page.":
+                break
+        elif ch =="F" and acc == 1:
             if line[i:i+35] == "Facebook  Twitter  Flipboard  Email":
-                Valid = False
-        if ch == "l" and acc == 0:
+                break
+        elif ch == "l" and acc == 0:
             if line[i-34:i+1] == "Facebook  Twitter  Flipboard  Email":
                 cleanline = ""
                 acc += 1
-        if Valid:
-            if ch.isalpha():
-                ch = ch.lower()
-                cleanline += ch
-            else:
-                cleanline += " "
+                continue
+        if ch.isalpha():
+            cleanline += ch.lower()
         else:
-            break
-    cleanlist = cleanline.split()
-    return cleanlist
+            cleanline += " "
+    return cleanline.split()
+
